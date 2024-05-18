981a2e69b0e987b0db29728280c9ba2cf3adc0c1
rafizul896 <rafizulislam896@gmail.com>
2024-05-18 17:53:04 +0600

    bistro boss server ready

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config()
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(express.json())
+app.use(cors())
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.USER_PASS}@cluster0.y7qmkns.mongodb.net/?retryWrites=true&w=majority&appName=cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+
+        // await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+    }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('bistro boss server is running..!')
+})
+
+app.listen(port, () => {
+    console.log(`Bistro boss server is runnung port on : ${port}`)
+})
